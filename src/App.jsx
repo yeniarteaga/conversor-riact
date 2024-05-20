f8ad416cc773bf9e789fe30c8d71ee385a48e1fc
Your Name <you@example.com>
2024-05-20 14:37:51 -0500

    "crear inicio de secion y conversor de TTS Y SST"

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState} from 'react'
+import Conversor from './conversor'
+
+function App() {
+  // hook,
+  const [usuario, setUsuario] = useState('') //estado para gurdar usuario
+  const [clave, setClave] = useState('') //estado para gurdar clave
+  const [logueado, setLogueado] = useState(false) //estado para saber se el usuario esta logueado
+
+  //funcion para cambiar el valor del usuario
+  function cambiarUsuario (evento) {
+    setUsuario(evento.target.value)
+  }
+  //funcion para cambiar el valor de la calve
+  function cambiarClave(evento) {
+    setClave(evento.target.value)
+  }
+
+  //funcion para ingreasar al dar click en el boton
+  function ingresar() {
+    console.log('Usuario:', usuario)
+    console.log('Clave:', clave)
+    if (usuario === 'admin' && clave === 'admin') { // si el usuario y la clave son admin    
+      alert("Datos correctos")
+      setLogueado (true)
+    } else { // si el usurio y la clave no son admin
+      alert("Datos incorrectos")
+    }
+  }
+
+  if (logueado) {
+    return(<Conversor/>)
+  }
+
+  return (
+  <>
+    <h1>Inicio de sesión</h1>
+      <label htmlFor='usuario'>Usuario:
+        <input id='usuario' type="text" value={usuario} onChange={cambiarUsuario}/>
+      </label>
+      <label htmlFor="clave">Clave:
+        <input id= 'calve'type="password"value={clave} onChange={cambiarClave}/>
+      </label>
+      <button type="submit" onClick={ingresar} >Ingresar</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/conversor.jsx
@@ -0,0 +1,44 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState } from 'react'
+
+function conversor() {
+  const [texto, setTexto] = useState('')
+  const [voz, setvoz] = useState('')
+
+  function cambiarTexto (evento) {
+    setTexto (evento.target.value)
+  }
+  
+  function textoAVoz() {
+    const configuracion = new SpeechSynthesisUtterance(texto) 
+    speechSynthesis.speak(configuracion) 
+  }
+
+  function vozATexto() {
+    const agente =new webkitSpeechRecognition()
+    agente.start()
+      agente.onresult = resultado
+  }
+  function resultado (informacion) {
+    console.log(informacion.results[0][0].transcript)
+      setVoz(informacion.results[0][0].transcript)
+      //informacion.results[0][@].transcript
+  }
+
+    return (
+      <> 
+        <h1>conversor TTS y STT</h1>
+        <h2>conversor texto a voz</h2>
+        <input type="text" value={texto} onChange={cambiarTexto} />
+        <button onClick={textoAVoz} >Convertir</button>
+        <h2>onversor voz a textox</h2>
+        <button onclick = {textoAVoz}>Grabar</button>
+        {Voz}
+      </>
+    )
+  }
+
+export default conversor
