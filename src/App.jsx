b96041dfbfd170c1d74df4163fc4f8904c415844
Your Name <you@example.com>
2024-06-18 09:38:09 -0500

    se creo registro, visualizacion y modulacion de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import {useState,useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Conversor from './Conversor'
+import Usuarios from './Usuarios'
+import Registro from './Registro'
 
 function App() {
-  const [usuario, setUsuario] = useState('') 
-  const [clave, setClave] = useState('') 
-  const [logueado, setLogueado] = useState(false) 
+  const [usuario, setUsuario] = useState('')
+  const [clave, setClave] = useState('')
+  const [logueado, setLogueado] = useState(false)
+  const [recargar, setRecargar] = useState(false)
 
-  function cambiarUsuario (evento) {
+  function cambiarUsuario(evento) {
     setUsuario(evento.target.value)
   }
 
@@ -17,42 +18,48 @@ function App() {
     setClave(evento.target.value)
   }
 
+  function recargarAhora() {
+    setRecargar(!recargar)
+
+  }
+
   async function ingresar() {
-    const peticion = await fetch('http://localhost:3000/login?usuario='+ usuario + '&clave=' + clave,{credentials:'include'})
+    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include' })
     if (peticion.ok) {
       setLogueado(true)
-    }else {
+    } else {
       alert('usuario o clave incorectos')
     }
-    //if (usuario == 'admin' && clave == 'admin') {   
-      //alert('Ingresaste')
-      //setLogueado (true)
-    //} else { 
-      //alert('Usuario o clave incorrectos')
-    //}
   }
 
   async function validar() {
     const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' })
     if (peticion.ok) {
-    setLogueado(true)
+      setLogueado(true)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     validar()
-  },[])
+  }, [])
 
-    if (logueado) {
-    return <Conversor />
+  if (logueado) {
+    return (
+
+      <>
+        <Registro recargarAhora={recargarAhora} />
+        <Conversor />
+        <Usuarios recargar={recargar} />
+      </>)
   }
 
   return (
     <>
-    <h1>Inicio de sesion</h1>
-      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario}/>
-      <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave}/>
+      <h1>Inicio sesion</h1>
+      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
+      <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
       <button onClick={ingresar}>Ingresar</button>
+
     </>
   )
 }
---
+++ src/Usuarios.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function Usuario(recargar) {
+  const [usuarios, setUsuarios] = useState([])
+
+  async function obtenerusuarios() {
+    const peticion = await fetch('http://localhost:3000/usuarios', { credentials: 'include' })
+    if (peticion.ok) {
+      const repuesta = await peticion.json()
+      setUsuarios(repuesta)
+    }
+  }
+
+  async function eliminarusuario(id) {
+    const peticion = await fetch('http://localhost:3000/usuarios?id=' + id, { credentials: 'include', method: 'DELETE' })
+    if (peticion.ok) {
+      alert('usuario eliminado')
+      obtenerusuarios();
+    }
+  }
+
+
+  useEffect(() => {
+    obtenerusuarios()
+  }, [recargar])
+
+  return (
+    <>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Usuario</th>
+            <th>Clave</th>
+            <th>opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            usuarios.map(usuario => (
+              <tr key={usuario.id}>
+                <th>{usuario.id}</th>
+                <th>{usuario.usuario}</th>
+                <th>{usuario.clave}</th>
+                <th>
+                  <button
+                    onClick={() => { eliminarusuario(usuario.id) }}
+                  >x</button>
+                </th>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+    </>
+  )
+}
+
+export default Usuario
