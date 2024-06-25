5535d2b7b851bfa9fa92b7a7a9162405af02c989
Your Name <you@example.com>
2024-06-24 20:31:15 -0500

    cambio de nombre conversor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import Conversor from './Conversor'
+import Conversor from './Conversor1'
 import Usuarios from './Usuarios'
 import Registro from './Registro'
 
